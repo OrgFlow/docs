6e191fe1784a336fbc0d5643bb3e581bc3f78a92
Chris <chris@orgflow.io>
2021-01-30 14:42:50 +0000

    Adds leap template

---
+++ templates/leap/conceptual.extension.js
@@ -0,0 +1,16 @@
+var common = require('./common.js');
+var util = require('./statictoc.util.js');
+
+/**
+ * This method will be called at the start of exports.transform in conceptual.html.primary.js
+ */
+exports.preTransform = function (model) {
+    return model;
+  }
+  
+  /**
+   * This method will be called at the end of exports.transform in conceptual.html.primary.js
+   */
+  exports.postTransform = function (model) {
+    return model;
+  }
