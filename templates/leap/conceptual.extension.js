b1966fa224edca2de5c995039f1e64d67f055bd4
Chris Payne <2872984+paynecrl97@users.noreply.github.com>
2021-09-15 19:40:10 +0100

    Restructures documentation to fit with the new structure
    * Moves files into new folder structure
    
    * Adds recursive menus
    
    * Improves home page menu and vertical scrolling
    
    * Orders glossary terms
    
    * Fixes termynal spacing
    
    * Further restructure for reference articles

--- templates/leap/conceptual.extension.js
+++ templates/leap/conceptual.extension.js
@@ -9,5 +9,45 @@ exports.preTransform = function (model) {
  * This method will be called at the end of exports.transform in conceptual.html.primary.js
  */
 exports.postTransform = function (model) {
+	for (var i = 0; i < model._toc.items.length; i++) {
+		var item = model._toc.items[i];
+
+		item.isTopLevel = true;
+		item.firstChildHref = getFirstChildHref(item);
+
+		traverseItems(model.uid, model.isHomepage, item);
+	}
+
 	return model;
+}
+
+function traverseItems(uid, isHomepage, item) {
+	var isActive = uid && item.topicUid === uid;
+
+	item.hasChildren = item.items.length > 0;
+
+	for (var i = 0; i < item.items.length; i++) {
+		isActive = traverseItems(uid, isHomepage, item.items[i]) || isActive;
+	}
+
+	item.active = isActive;
+	item.displayInSideMenu = isActive || isHomepage;
+
+	return isActive;
+}
+
+function getFirstChildHref(item) {
+	if (item.href) {
+		return item.href;
+	}
+
+	for (var i = 0; i < item.items.length; i++) {
+		var href = getFirstChildHref(item.items[i]);
+
+		if (href) {
+			return href;
+		}
+	}
+
+	return null;
 }
